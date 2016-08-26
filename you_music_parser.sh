44ca9ed5a9f8ac73e6038835ad951ee6b6c01f3c
Jiri Caga <jiri.caga30@gmail.com>
2016-08-26 07:50:44 +0200

    remove unused file

--- you_music_parser.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
- 
-line=$1;
-album=$(echo ${line} | cut -d ';' -f 1);
-song=$(echo ${line} | cut -d ';' -f 2);
-url=$(echo ${line} | cut -d ';' -f 3);
-youtube-dl --output "Music/${album}/${song}.%(ext)s" --playlist-items 1 --extract-audio --audio-format mp3 --prefer-ffmpeg ${url} | tee -a Music/log.txt;
